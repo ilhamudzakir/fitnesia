88ec3ed7a0cc4fe12a89617e167f3f3737b8b846
ilhamudzakir <ilhamudzakir@gmail.com>
2017-05-06 21:55:38 +0700

    updated
    <br>

--- application/views/frontend/career/index.php
+++ application/views/frontend/career/index.php
@@ -6,3 +6,49 @@
     </div>
     </div>
 </section>
+
+<section id="career">
+	<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<h5>Featured Jobs</h5>
+			<div class="content-career">
+				<span>Data Analyst</span><br>
+				<span>Information Security Analyst</span><br>
+				<span>Sales Marketing</span><br>
+				<span>Customer Sevice</span><br>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<h5>Department</h5>
+			<div class="content-career">
+				<span>Engineering</span><br>
+				<span>Engineering</span><br>
+				<span>Sales</span><br>
+				<span>Customer Sevice</span><br>
+				</div>
+		</div>
+		<div class="col-md-4">
+			<h5>Location</h5>
+			<div class="content-career">
+				<span>Jakarta</span><br>
+				<span>Jakarta</span><br>
+				<span>Jakarta</span><br>
+				<span>Jakarta</span><br>
+			</div>
+		</div>
+		<div class="col-md-12 text-center title2-career">
+			<h5>Send us your application</h5>
+		</div>
+		<br>
+		<div class="col-md-6 content-career text-center">
+			<span>Event if you don't see a current opening that suits your career objectives, send us your resume. We are always looking for the best talent.</span>
+		</div>
+		<div class="col-md-6 content-career text-center">
+		<span> Send your application / CV to</span><br>
+		<b>hr@satuplatform.com</b>
+		</div>
+	</div>
+		
+	</div>
+</section>
